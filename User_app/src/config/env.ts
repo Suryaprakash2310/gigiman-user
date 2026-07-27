d91107ecaeea14c928c2da3c524f1d214cc73e97
Suryaprakash2310 <suryaprakash33207@gmail.com>
2026-07-27 17:35:20 +0530

    final changes before build

--- User_app/src/config/env.ts
+++ User_app/src/config/env.ts
@@ -1,3 +1,3 @@
-export const API_BASE_URL = process.env.API_BASE_URL || 'https://gigiman1.onrender.com/api';
-export const SOCKET_URL = process.env.SOCKET_URL || 'https://gigiman1.onrender.com';
+export const API_BASE_URL = process.env.API_BASE_URL || 'https://api.gigiman.in/api';
+export const SOCKET_URL = process.env.SOCKET_URL || 'https://api.gigiman.in';
 export const MAPBOX_ACCESS_TOKEN = process.env.EXPO_PUBLIC_MAPBOX_TOKEN!;
--- User_app/src/navigation/AppStack.tsx
+++ User_app/src/navigation/AppStack.tsx
@@ -1,14 +1,14 @@
 // src/navigation/BottomTabs.tsx
-import React, { useEffect } from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import React, { useEffect } from 'react';
 
-import HomeStack from './stacks/HomeStack';
-import BookingStack from './stacks/BookingStack';
-import ServiceStack from './stacks/ServiceStack';
-import ProfileStack from './stacks/ProfileStack';
-import { useTheme } from '../theme/useTheme';
-import CustomTabBar from '../components/ui/CustomTabBar';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
+import CustomTabBar from '../components/ui/CustomTabBar';
+import { useTheme } from '../theme/useTheme';
+import BookingStack from './stacks/BookingStack';
+import HomeStack from './stacks/HomeStack';
+import ProfileStack from './stacks/ProfileStack';
+import ServiceStack from './stacks/ServiceStack';
 
 export type AppTabsParamList = {
   HomeTab: undefined;
@@ -20,24 +20,26 @@ export type AppTabsParamList = {
 const Tab = createBottomTabNavigator<AppTabsParamList>();
 
 export default function AppStack() {
-  const { setMode, theme  } = useTheme();
+  const { setMode, theme } = useTheme();
   const insets = useSafeAreaInsets();
   useEffect(() => {
     setMode?.('light');
   }, []);
   return (
-    <Tab.Navigator screenOptions={{ headerShown: false,tabBarStyle: {
-          height: 65 + insets.bottom,
-          paddingBottom: insets.bottom + 8,
-          paddingTop: 12,
-          backgroundColor: theme.colors.surface,
-          borderTopWidth: 0,
-          elevation: 10,
-        }, }}
+    <Tab.Navigator screenOptions={{
+      headerShown: false, tabBarStyle: {
+        height: 65 + insets.bottom,
+        paddingBottom: insets.bottom + 8,
+        paddingTop: 12,
+        backgroundColor: theme.colors.surface,
+        borderTopWidth: 0,
+        elevation: 10,
+      },
+    }}
       tabBar={(props) => <CustomTabBar {...props} />} >
       <Tab.Screen name="HomeTab" component={HomeStack} />
-      <Tab.Screen name="BookingTab" component={BookingStack} />
       <Tab.Screen name="ServiceTab" component={ServiceStack} />
+      <Tab.Screen name="BookingTab" component={BookingStack} />
       <Tab.Screen name="ProfileTab" component={ProfileStack} />
     </Tab.Navigator>
   );
